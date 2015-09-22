bc6fe66e7981327f8d3f9fcf29ce4fc70708e1a8
Jacob Sanders <j.q.sanders@gmail.com>
2015-09-21 22:57:56 -0600

    I beat you, JavaScript. Today, I beat you.

--- main/static/js/cast_crew.js
+++ main/static/js/cast_crew.js
@@ -68,12 +68,16 @@ for(var i = 0; i < workerLinks.length; i++) {
     })
 };
 
+
+
 for(var i = 0; i < workerLinks.length; i++) {
     workerLinks[i].addEventListener('mouseenter', function(e) {
-        var cameras = this.getAttribute('data-number');
-        for (var x = 1; x <= cameras; x++) {
-            var img = document.getElementById('movie' + x)
-            img.src = '/static/img/movie-camera-red.png';
+        if (document.getElementsByClassName('crew-name-black').length == 0) {
+            var cameras = this.getAttribute('data-number');
+            for (var x = 1; x <= cameras; x++) {
+                var img = document.getElementById('movie' + x)
+                img.src = '/static/img/movie-camera-red.png';
+            }
         };
     })
 };
